4b6f9a1b08a896648f1d70d8df354512a63cbbd3
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2025-10-02 18:01:53 +0200

    added lazy error page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import Dashboard from "./components/dashboard/Dashboard";
+import ErrorPage from "./components/ErrorPage";
 
 
 const queryClient = new QueryClient();
@@ -12,8 +13,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route index element={<Navigate to="/contacts/r/" replace />} />
-          <Route path="/contacts/r/" element={<Dashboard />} />
+          <Route path="/contacts/r/">
+            <Route index element={<Dashboard />} />
+            <Route path="*" element={<ErrorPage errorCode={404} message="Page Not Found" />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/contacts/r/" replace />} />
         </Routes>
       </Router>
       <ReactQueryDevtools initialIsOpen={false} />
